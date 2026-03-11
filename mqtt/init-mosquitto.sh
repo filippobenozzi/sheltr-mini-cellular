068e36740134e7c7d6d174848e181f34f90cfaca
Filippo Benozzi <email@filippo.im>
2026-03-11 19:09:21 +0100

    initial

--- mqtt/init-mosquitto.sh
+++ mqtt/init-mosquitto.sh
@@ -3,16 +3,18 @@ set -eu
 
 USERNAME="${MQTT_USERNAME:-filippo}"
 PASSWORD="${MQTT_PASSWORD:-filippo1994}"
-PASSFILE="/mosquitto/config/passwords.txt"
+PASSFILE="/mosquitto/data/passwords.txt"
 
 if [ -z "$USERNAME" ] || [ -z "$PASSWORD" ]; then
   echo "MQTT_USERNAME e MQTT_PASSWORD devono essere valorizzati." >&2
   exit 1
 fi
 
-TMP_PASSFILE="$(mktemp)"
+TMP_DIR="$(mktemp -d)"
+TMP_PASSFILE="$TMP_DIR/passwords.txt"
 mosquitto_passwd -b -c "$TMP_PASSFILE" "$USERNAME" "$PASSWORD"
 mv "$TMP_PASSFILE" "$PASSFILE"
 chmod 600 "$PASSFILE"
+rmdir "$TMP_DIR"
 
 exec /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
